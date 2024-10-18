c9fdda932794b9026d1d4655cf40a4e434cc0520
sharvan <sharvan@fxcareers.com>
2024-10-18 15:05:31 +0530

    first-comit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Component/Header"
+
+function App() {
+  return (
+   <main className='overflow-hidden'>
+    <Header/>
+   </main>
+  )
+}
+
+export default App
---
+++ src/Component/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <a href="">
+                    <img src="/images/xora.svg" height={55} width={115} alt="" />
+                </a>                                                                                                                                                                            
+            </div>
+        </header>
+    )
+}
+
+export default Header
